9c7af1df3567dc57a726c9b24835b278e6b0591b
Andrei Korzhevskii <a.korzhevskiy@gmail.com>
2017-11-19 17:04:33 +0400

    Install gwt-stripper and closure compiler unshaded to local repo.

--- maven-push.sh
+++ maven-push.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Tweak these based on the path to your j2cl, your preferred groupid
-J2CL_REPO=/Users/colin/workspace/j2cl
+source config.sh
 GROUP_ID=com.vertispan.j2cl
 
 GOAL='install:install-file'
@@ -21,10 +21,14 @@ fi
 mvn $GOAL -DrepositoryId=vertispan-releases -Durl=https://repo.vertispan.com/j2cl -Dfile=bootstrap.js.zip -DgroupId=$GROUP_ID -DartifactId=bootstrap -Dversion=0.1-SNAPSHOT -Dtype=zip -Dclassifier=jszip -Dpackaging=zip
 
 # Transpiler itself (and eventually other jars
-pushd $J2CL_REPO/transpiler
+#pushd $J2CL_REPO/transpiler
 # TODO this doesn't respect the GROUPID above, you'll need to tweak this too
-mvn clean install -Ppackage
-popd
+#mvn clean install -Ppackage
+#popd
+
+mvn $GOAL -DrepositoryId=vertispan-releases -Durl=https://repo.vertispan.com/j2cl -Dfile=$J2CL_REPO/bazel-bin/tools/java/com/google/j2cl/tools/gwtincompatible/GwtIncompatibleStripper.jar -DgroupId=$GROUP_ID -DartifactId=gwt-incompatible-stripper -Dversion=0.1-SNAPSHOT -Dpackaging=jar
+mvn $GOAL -DrepositoryId=vertispan-releases -Durl=https://repo.vertispan.com/j2cl -Dfile=$J2CL_REPO/bazel-bin/transpiler/java/com/google/j2cl/transpiler/J2clTranspiler.jar -DgroupId=$GROUP_ID -DartifactId=transpiler -Dversion=0.1-SNAPSHOT -Dpackaging=jar
+mvn $GOAL -DrepositoryId=vertispan-releases -Durl=https://repo.vertispan.com/j2cl -Dfile=$WORKSPACE/closure-compiler/target/closure-compiler-unshaded-1.0-SNAPSHOT.jar -DgroupId=com.vertispan.javascript -DartifactId=closure-compiler-unshaded -Dversion=1.0-SNAPSHOT -Dpackaging=jar
 
 # JRE
 pushd $J2CL_REPO/bazel-bin/jre/java
